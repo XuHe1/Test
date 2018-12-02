76f69c212eb5d8353a01cca8174ef6f96f91ad01
xuhe <xuhe@xuhedeMBP.lan>
2018-12-02 21:48:59 +0800

    update

--- src/main/java/top/lovelily/concurrent/TestStock.java
+++ src/main/java/top/lovelily/concurrent/TestStock.java
@@ -212,8 +212,32 @@ public class TestStock {
         }
     }
 
-
-
+    /**
+     * HMSET goodsId total 100 booked 0
+     * "goodsId" : {
+     *     "total": 100
+     *     "booked": 100
+     *  }
+     *
+     * src/redis-cli script load "$(cat spike.lua)"
+     * local n = tonumber(ARGV[1])
+     * if not n  or n == 0 then
+     *     return 0
+     * end
+     * local vals = redis.call("HMGET", KEYS[1], "total", "booked");
+     * local total = tonumber(vals[1])
+     * local blocked = tonumber(vals[2])
+     * if not total or not blocked then
+     *     return 0
+     * end
+     * if blocked + n <= total then
+     *     redis.call("HINCRBY", KEYS[1], "booked", n)
+     *     return n;
+     * end
+     * return 0
+     *
+     *
+     */
 
 
 

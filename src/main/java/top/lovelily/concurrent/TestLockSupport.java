e543a0270c4ea369f1cc890ed5fd8eaedcbebefb
xuhe <1090624761@qq.com>
2024-02-29 17:36:34 +0800

    update

---
+++ src/main/java/top/lovelily/concurrent/TestLockSupport.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.locks.LockSupport;
+
+public class TestLockSupport {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("开始执行");
+                LockSupport.park();
+                System.out.println("结束执行");
+
+            }
+        });
+        t1.start();
+
+        //LockSupport.unpark(t1);
+       // t1.interrupt();
+
+
+    }
+}

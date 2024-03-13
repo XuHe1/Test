d0188e3c8ff051463ef01b965826c9aaae8a48fd
xuhe <1090624761@qq.com>
2024-03-13 16:17:02 +0800

    update

--- src/main/java/top/lovelily/TestExamChinTel.java
+++ src/main/java/top/lovelily/TestExamChinTel.java
@@ -12,11 +12,14 @@ public class TestExamChinTel {
     // （匿名）内部类不能定义 static 方法和变量？
     // interface 方法定义
     // float f = 1.1; double d = 2.15e12;
-    // 泛型的优点？类型安全，不需要类型转换？运行快？省去装箱拆箱？
+    // 泛型的优点？类型安全：编译时就能做类型检查，减少了运行时类型转换异常。2. 不需要类型转换：运行时无需类型转换。3. 运行快：可以避免运行时类型转换，运行更快。省去装箱拆箱？
     // 反射能获取私有变量和方法吗？
     // 先执行 try 中的return 还是 finally 中先打印
     // ReentrantLock 是互斥锁吗？
     // lamda 只能定义一个抽象方法，函数接口
+    // 抽象工厂是对工厂模式的抽象吗？
+    // Thread.stop()会导致线程休眠？
+    // 数字开头5个数字：^\d{5}    ^[\d]{5}
 
     int sum(int a, int b) {
         try{
@@ -69,5 +72,11 @@ class B extends A{
         protected int c();
         abstract int d();
         final int e();
+        static void f(){};
     }
+
+    /**
+     * protected: 子类可以访问，子类和父类在不同包；
+     *
+     */
 }

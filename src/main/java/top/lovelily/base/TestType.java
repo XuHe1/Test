0d84e3a7780781bfbd380a0c3901fc98a4c19b49
xuhe <1090624761@qq.com>
2024-08-07 21:09:41 +0800

    update

--- src/main/java/top/lovelily/base/TestType.java
+++ src/main/java/top/lovelily/base/TestType.java
@@ -24,10 +24,16 @@ public class TestType {
         long c = a * b;
         System.out.println(c); // -2
 
-
+        // 默认双精度，所以d可以忽略
+        // double pi = 3.14f; // 单精度可以转双精度
+        // double pi = 3.14d
+        // double pi = 3.14;
         double d = 138.918248864067381; // 小数点后14位
         System.out.println(d);
 
+        //单精度 float pi=3.14f  f不能去掉
+        // float pi = 3.14d; 编译错误，丢失精度
+        // float pi = 3.14; 编译错误，默认是double双精度（3.14d），转单精度报错
         float f = 138.91824886406738f; // 小数点后5位
         System.out.println(f);
 

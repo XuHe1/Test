c385498004b252c74b304e4010e35b882cf25231
xuhe <1090624761@qq.com>
2024-12-05 19:02:24 +0800

    simple factory

--- src/main/java/top/lovelily/designpattern/factory/ShapeFactory.java
+++ src/main/java/top/lovelily/designpattern/factory/ShapeFactory.java
@@ -1,12 +1,14 @@
 package top.lovelily.designpattern.factory;
 
 /**
+ * 简单工厂模式
  * 只能生产一种产品（对象），比如”形状“
+ * 对客户端隐藏具体对象（Shape）的子类
  */
 public class ShapeFactory {
-    public factory.Shape getShape(String type) {
+    public Shape getShape(String type) {
         if ("Rectangle".equals(type)) {
-            return new factory.Rectangle();
+            return new Rectangle();
         }
         if ("Circle".equals(type)) {
             return new Circle();
--- src/main/java/top/lovelily/designpattern/factory/absfactory/Test.java
+++ src/main/java/top/lovelily/designpattern/factory/absfactory/Test.java
@@ -1,4 +1,11 @@
 package top.lovelily.designpattern.factory.absfactory;
 
 public class Test {
+    public static void main(String[] args) {
+        AbstractFactory factory = SuperFactory.getFactory("Shape");
+        factory.getShape("Circle");
+
+        AbstractFactory factory2 = SuperFactory.getFactory("Color");
+        factory2.getColor("Green");
+    }
 }

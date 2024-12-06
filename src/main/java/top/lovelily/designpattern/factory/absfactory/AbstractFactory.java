3459966e6780c059dd99b8f22b18f22cff23a62d
xuhe <1090624761@qq.com>
2024-12-06 17:03:49 +0800

    update

--- src/main/java/top/lovelily/designpattern/factory/absfactory/AbstractFactory.java
+++ src/main/java/top/lovelily/designpattern/factory/absfactory/AbstractFactory.java
@@ -3,6 +3,9 @@ package top.lovelily.designpattern.factory.absfactory;
 
 import top.lovelily.designpattern.factory.Shape;
 
+/**
+ * 抽象工厂：创建多个相关的对象（对象家族）
+ */
 public abstract class AbstractFactory {
     abstract Shape getShape(String type);
     abstract Color getColor(String color);
